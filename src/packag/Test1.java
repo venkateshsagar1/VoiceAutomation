056e50dd2edd205a1d5fcf20d71988d89813fd4b
Administrator <Administrator@192.168.0.2>
2019-01-07 21:12:39 +0530

    Hi surya

---
+++ src/packag/Test1.java
@@ -0,0 +1,12 @@
+package packag;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "D:\\MINDQ\\chromedriver.exe");
+		ChromeDriver driver=new ChromeDriver();
+	}
+
+}
